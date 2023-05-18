150a3e1bf8d677214313867d352c68973e3ed730
Rafael <rafael_guilherme@hotmai.com>
2023-05-18 15:36:08 -0300

    day 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,19 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-const port = 3333
-
-app.get('/hello', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+
+app.register(memoriesRoutes)
 
 app
   .listen({
-    port,
-    host: '0.0.0.0',
+    port: 3333,
   })
   .then(() => {
-    console.log(`HTTP server running on port ${port}`)
+    console.log('🚀 HTTP server running on port http://localhost:3333')
   })
